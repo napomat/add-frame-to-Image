0f9cb6d97785d10b7856319afa4fc031d65b5a7e
napomat <github@napomat.de>
2021-01-08 23:27:09 +0100

    Readme aktualisiert.

--- logic.ps1
+++ logic.ps1
@@ -9,50 +9,48 @@ $aspectratioB = $null
 $Folder = $null
 $Files = $null
 $File = $null
+$aspectratioselect = $null
 
 Write-Host -ForegroundColor Red -BackgroundColor Yellow  "`t`t Bitte das Seitenverhaeltnis waehlen!"
-$aspectratioselect = Read-Host -Prompt "0 = 7x5 , 1 = 7x9.5 , 2 = 7x7 , 3 = 9.5x14.5 " 
+$aspectratioselect = Read-Host -Prompt "0 = 7x5.5 , 1 = 7x9.5 , 2 = 7x7 , 3 = 9.5x14.5 " 
 
 
 ################Variables################
+$Bordercolor = "#eae4dd"
+
 
 switch ( $aspectratioselect ){
   0 { # 7x5,5
     $BordersizeA = "97x77.6"
     $BordersizeB = "77.6x97"
-    $Bordercolor = "#eae4dd"
     $aspectratioA = "1:0.78"
     $aspectratioB = "0.78:1"  }
   1 { #7x9,5
     $BordersizeA = "97x71,78"
     $BordersizeB = "71,78x97"
-    $Bordercolor = "#eae4dd"
     $aspectratioA = "1:0,74"
     $aspectratioB = "0,74:1"  }
   2 { # 7x7
     $BordersizeA = "97x97"
     $BordersizeB = "97x97"
-    $Bordercolor = "#eae4dd"
     $aspectratioA = "1:1"
     $aspectratioB = "1:1"  }
   3 { # 9,5x14,5
     $BordersizeA = "97x63.05"
     $BordersizeB = "63.05x97"
-    $Bordercolor = "#eae4dd"
     $aspectratioA = "0.65:1"
     $aspectratioB = "1:0.65" }
 }
 
 
-
 Function Get-Folder($initialDirectory="")
-
 {
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
 
     $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
     $foldername.Description = "Select a folder"
     $foldername.rootfolder = "MyComputer"
+    $foldername.ShowNewFolderButton = $false
     $foldername.SelectedPath = $initialDirectory
 
     Write-Host -ForegroundColor Red -BackgroundColor Yellow  "`t`tW A R N I N G :   Dialog box may be hidden behind another [or this] window!!!" 
@@ -60,8 +58,12 @@ Function Get-Folder($initialDirectory="")
     if($foldername.ShowDialog() -eq "OK")
     {
         $folder += $foldername.SelectedPath
+        return $folder
+    }else{
+      exit
     }
-    return $folder
+    
+    
 }
 
 $Folder = Get-Folder
@@ -79,7 +81,6 @@ foreach($File in $Files){
   $outname = $File.Name.ToString()+"_"+$(Get-Date).Ticks.ToString() + ".jpg"
 
   $ratio = . $imgmagic\magick.exe identify -format "%[fx:w]x%[fx:h]" $File.FullName
-
   $ratiosplit = $ratio.split("x")
 
   if ($ratiosplit[0] -lt $ratiosplit[1]){
@@ -96,3 +97,4 @@ remove-item $Folder\with_border\temp.jpg
 }
 Invoke-Item $Folder\with_border
 
+exit
